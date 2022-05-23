eb8be575e069abb67c82946e3d06e8b01043efec
Valeriy Pogorelov <valeriy.pogorelov@together.com>
2022-05-24 00:29:47 +0300

    server: add pages and races

---
+++ nest/src/races/races.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { RacesService } from './races.service';
+import { RacesResolver } from './races.resolver';
+import {racesProviders} from "./races.providers";
+import {PagesModule} from "../pages/pages.module";
+
+@Module({
+  providers: [
+    RacesService,
+    RacesResolver,
+    ...racesProviders,
+  ],
+  imports: [
+    PagesModule
+  ]
+})
+export class RacesModule {}
